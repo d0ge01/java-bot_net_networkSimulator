8c46d88d4279cda7c0e0ffe91f429c7392b1cf31
d0ge01 <not.d0ge@anche.no>
2013-09-25 19:53:59 +0200

    meh

--- src/it/d0ge01/botnet/util/util.java
+++ src/it/d0ge01/botnet/util/util.java
@@ -3,11 +3,11 @@ package it.d0ge01.botnet.util;
 public class Util {
 	public static double getRandTime() {
 		double x = -1 * log10(Math.random())*Math.sqrt(900.0);
-		return(1);
+		return(x);
 	}
 	
 	public static double log10(double d) {
-	    return Math.log(d)/Math.log(10.0);
+	    return ((double) Math.log(d)/Math.log(10.0));
 	}
 	
 	
