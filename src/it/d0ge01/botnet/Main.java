593a3a0fbac2a109b1b487873d30a6b44319a478
d0ge01 <not.d0ge@anche.no>
2013-09-26 13:14:17 +0200

    added netpool first commit

--- src/it/d0ge01/botnet/Main.java
+++ src/it/d0ge01/botnet/Main.java
@@ -1,11 +1,17 @@
 package it.d0ge01.botnet;
 
+import it.d0ge01.botnet.net.*;
+import it.d0ge01.botnet.util.*;
+
 public class Main {
 	final static boolean DEBUG = true;
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		NetPool field = new NetPool(100);
+		
+		field.startNetPool();
 		
 	}
 	
--- src/it/d0ge01/botnet/net/NetPool.java
+++ src/it/d0ge01/botnet/net/NetPool.java
@@ -3,13 +3,26 @@ package it.d0ge01.botnet.net;
 import it.d0ge01.botnet.Main;
 
 public class NetPool {
-	NetNode[] field = new NetNode[100];
+	private int sizeNet = 100;
+	private NetNode[] field = new NetNode[sizeNet];
+	
+	public NetPool(int size) {
+		if ( size > 0 )
+			this.sizeNet = size;
+	}
 	
 	public void startNetPool() {
 		if ( Main.debug())
 			System.out.println("Resetting Net");
 		
-		for ( int i = 0 ; i < 100 ; i++ )
-			field[i] = new Host("hostname" + i , "192.168.1" + i);
+		for ( int i = 0 ; i < 100 ; i++ ) {
+			if ( Main.debug())
+				System.out.print("Creating node ( hostname" + i + ", 192.168.1." + i + " ) ....   "); 
+			
+			field[i] = new Host("hostname" + i , "192.168.1." + i);
+			
+			if ( Main.debug())
+				System.out.println("DONE");
+		}
 	}
 }
