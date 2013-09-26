576579330ba7c18df354dde84258a312d1cf62a7
d0ge01 <not.d0ge@anche.no>
2013-09-26 14:19:19 +0200

    thread :/

--- src/it/d0ge01/botnet/Main.java
+++ src/it/d0ge01/botnet/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		field = new NetPool(100);
+		Thread r = new Thread(field);
 		field.startNetPool();
 		
 		console();
