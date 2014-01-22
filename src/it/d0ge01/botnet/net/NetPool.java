8c4555a8df03b0b9eda3487ff5f92fb42c7cfd3f
d0ge01 <not.d0ge@anche.no>
2014-01-22 09:19:00 +0100

    changed to list

--- src/it/d0ge01/botnet/net/NetPool.java
+++ src/it/d0ge01/botnet/net/NetPool.java
@@ -4,75 +4,57 @@ import it.d0ge01.botnet.Main;
 import it.d0ge01.botnet.Malware;
 
 public class NetPool extends Thread {
-	public int sizeNet = 255;
-	private NetNode[] field = new NetNode[sizeNet];
+  private LinkedList<NetNode> field[];
 	private String netAddr = "192.168.1.";
-	
-	public NetPool(int size, String netAddr) {
-		if ( size > 0 )
-			this.sizeNet = size;
+
+	public NetPool(String netAddr) {
+    this.field = new LinkedList();
 		this.netAddr = netAddr;
 	}
-	
-	public void startNetPool(int bm) {
-		if ( Main.debug())
-			System.out.println("Resetting Net");
-		
-		for ( int i = 0 ; i < sizeNet ; i++ ) {
-			if ( Main.debug())
-				System.out.print("Creating node ( hostname" + i + ", " + this.netAddr + i + " ) ....   "); 
-			
-			field[i] = new NetNode("hostname" + i , this.netAddr + i);
-			
-			if ( Main.debug())
-				System.out.println("DONE");
-		}
-		
-		if ( Main.debug())
-			System.out.print("Creating node ( botmaster , " + this.netAddr + bm + ") ....   "); 
-		
-		
-		field[bm] = new BotmasterHost("192.168.1." + bm);
-		
-		if ( Main.debug())
-			System.out.println("DONE");
-	}
-	
+
+  public void addHost(int size) {
+    if ( Main.debug() )
+      System.out.println("Resetting Net");
+    for ( int i = 0 ; i < size ; i++ )
+      field.add(new NetNode("hostname" + i, this.netAddr + i));
+
+  }
+
 	public NetNode host(int i) {
 		return field[i];
 	}
-	
-	public NetNode hostByIp(int last) {
-		return field[last];
+
+	public NetNode hostByIp(int n) {
+		return ((NetNode) field.get(n));
 	}
-	
+
 	public NetNode hostByHostname(String hostname) {
-		for ( int i = 0 ; i < sizeNet; i++ ) {
-			if ( field[i].hostname == hostname )
-				return field[i];
+		for ( int i = 0 ; i < field.size(); i++ ) {
+			if ( ((NetNode) field).hostname == hostname )
+				return ((NetNode) field.get(i));
 		}
 		// if fail return null :D
 		return null;
 	}
-	public void communication(NetNode x, NetNode y, String txt, NetPool netpool ) {
+	public void communication(NetNode x, NetNode y, String txt, NetPool netpool) {
 		if ( netpool == null )
 			netpool = this;
 		String buff = txt;
-		y.recv(netpool, buff, x);
+		x.send(y, txt);
 	}
-	
+
 	public void broadcast(NetNode from, String txt, NetPool net) {
 		for ( int i = 0 ; i < sizeNet ; i++ )
 			communication(from, field[i], txt, net);
 	}
-	
-	public boolean checkNet(NetNode n) {
+
+	public boolean IsThisNet(NetNode n) {
 		if ( n.getNetAddr() == this.netAddr )
 			return true;
 		return false;
 	}
-	
+
 	public void overrideHost(int i, NetNode x) {
-		field[i] = x;
+		field.set(x, i);
 	}
 }
